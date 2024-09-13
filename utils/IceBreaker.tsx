91da732c859cc995875bdd85ffdcb5279d736b83
Benjamin <57810427+btw04@users.noreply.github.com>
2024-09-13 14:49:53 +0200

    added questions

--- utils/IceBreaker.tsx
+++ utils/IceBreaker.tsx
@@ -1,10 +1,14 @@
-const questions: string[] = [
+const questions_ger: string[] = [
     "Was ist deine Lieblingsprogrammiersprache?",
     "Ananas auf Pizza?",
     "Auf welche Module freust du dich?",
-    "Mac, Windows oder Linux?",
-    "Kaffee oder Tee?",
+    "MacOS, Windows oder Linux?",
+    "Kaffee, Tee oder Mate?",
     "Ohne was könntest du nicht leben?",
+    "Was ist dein Lieblingsfilm?",
+    "Was ist dein Lieblingsbaum?",
+    "Ist Rust besser als C oder nicht?",
+    "Denkst du in Bildern oder Worten?",
     "Wie viele Tabs sind in deinem Browser normalerweise offen?",
     "Welcher Videospiel-Charakter wärst du gerne?",
     "Welche Musik hörst du beim Lernen?"
@@ -12,5 +16,5 @@ const questions: string[] = [
 
 
 export default function getQuestions(amount: number): string[] {
-    return questions.sort(() => Math.random() - 0.5).slice(0, amount);
+    return questions_ger.sort(() => Math.random() - 0.5).slice(0, amount);
 }
